91e3d4674ba0f569913a1c9b24cd3bd0ffc25c26
JoelHeuer <heuer.joel@hotmail.com>
2021-11-24 19:49:01 +0100

    deleted checkpoint a

--- src/dns_server.py
+++ src/dns_server.py
@@ -124,34 +124,6 @@ class DnsServer:
         )
         return df
 
-    ####################[checkpoint a]#############################
-    def get_record(self, name: str) -> str:
-        df = pd.read_csv(
-            self.zone_file,
-            sep="\t",
-            header=None,  # no column names in .zone-files
-            names=["name", "record"],
-        )
-
-        list_of_records = df.loc[df["name"] == name]["record"]
-        record = list_of_records.iloc[0]
-        return record
-
-    def checkpoint_a(self):
-        while True:
-            # receive msg
-            msg, client = self.nameserver.recvfrom(CONST.BUFFER)
-            msg = msg.decode("utf-8")
-
-            print(
-                f"server '{self.name}' received query: '{msg}' from {client}")
-
-            # search for record
-            record = self.get_record(name=msg)
-
-            # response to client
-            response = f"{msg} has record: {record}"
-            self.nameserver.sendto(str.encode(response), client)
 
     ####################[checkpoint b]#############################
 
--- src/stub_resolver.py
+++ src/stub_resolver.py
@@ -20,20 +20,6 @@ class StubResolver:
         self.client = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-    def checkpoint_a(self) -> None:
-        # input
-        input_auth_server = "homework.fuberlin"
-        input_server_of_interest = f"easy.{input_auth_server}"
-
-        # get ip adress
-        ip = CONST.get_ip(server_name=input_auth_server)
-
-        # send query to server
-        self.client.sendto(str.encode(
-            input_server_of_interest), (ip, CONST.PORT))
-        response, _ = self.client.recvfrom(CONST.BUFFER)
-        print(response.decode("utf-8"))
-
     def checkpoint_b(self) -> None:
         msg_request = str.encode(input("Enter message:"))
         rec_res_info = (CONST.IP_REC_RESOLVER, CONST.PORT)
